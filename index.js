5d1fcaba832cac8fc4db3afac3d59caff7316b95
Kaushik Kumaran <kumarankaushik@gmail.com>
2020-12-26 14:09:43 +0400

    database

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
+const express = require('express')
+require('./db/mongoose')
+const connectDB = require('./db/mongoose')
 const dotenv = require('dotenv')
+
 dotenv.config({path: "./config/dev.env"})
 
-const express = require('express')
 const app = express()
-
+connectDB()
 const port = process.env.PORT
+
+
 
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
     res.render('index')    
 })
+
+app.post('/shortUrl', (req, res) => {
+
+} )
 
 app.listen(port, () => {
     console.log('Wakey Wakey kk, Server is running on '+ port)
